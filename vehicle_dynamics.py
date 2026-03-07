d8109e36f036131c49d66dc38e41758e9fbcfaea
Tom Downey <trad3@cam.ac.uk>
2026-03-07 10:59:58 +0000

    Initial commit.

---
+++ vehicle_dynamics.py
@@ -0,0 +1,16 @@
+# import modules
+import numpy as np
+import matplotlib.pyplot as plt
+
+# main function
+def main():
+
+    # do nothing
+    pass
+
+# on script execution
+if __name__ == "__main__":
+
+    # run main
+    main()
+    
